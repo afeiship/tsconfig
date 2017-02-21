6f6827e630a503f6be904ac3ced7ee1dc6ebcff4
afeiship <1290657123@qq.com>
2017-02-21 23:23:05 +0800

    format

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 var nx = require('next-js-core2')
-var global = nx.GLOBAL
+var global = nx.GLOBAL;
 var Store = nx.declare('nx.Store', {
   statics: {
     engine: 'localStorage',
-    config: function (inOptions) {
-      nx.mix(Store, inOptions)
+    config:function(inOptions){
+        nx.mix(Store,inOptions);
     },
     set: function (inKey, inValue) {
-      global[Store.engine].setItem(inKey, nx.stringify(inValue))
+        global[Store.engine].setItem(inKey,nx.stringify(inValue));
     },
     get: function (inKey) {
-      var value = global[Store.engine].getItem(inKey)
-      return nx.parse(value)
+        var value = global[Store.engine].getItem(inKey);
+        return nx.parse(value);
     },
     clear: function (inKey) {
-      global[Store.engine].removeItem(inKey)
+        global[Store.engine].removeItem(inKey);
     },
     clearAll: function () {
-      global[Store.engine].clear()
+        global[Store.engine].clear();
     }
   }
-})
+});
 
-module.exports = Store
+module.exports=Store;
