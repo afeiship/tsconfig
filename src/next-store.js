4da1106ab75ccb1e996ca73d669ccec717d7d831
afeiship <1290657123@qq.com>
2017-02-21 23:40:24 +0800

    udpate dist

--- src/next-store.js
+++ src/next-store.js
@@ -1,26 +1,31 @@
-nx.declare('nx.Store', {
-  statics: {
-    engine: 'localStorage',
-    config:function(inOptions){
-        nx.mix(Store,inOptions);
-    },
-    set: function (inKey, inValue) {
-        global[Store.engine].setItem(inKey,nx.stringify(inValue));
-    },
-    get: function (inKey) {
-        var value = global[Store.engine].getItem(inKey);
-        return nx.parse(value);
-    },
-    sets:function(inObject){
-        nx.each(inObject,function(key,value){
-            Store.set(key,value);
-        });
-    },
-    clear: function (inKey) {
-        global[Store.engine].removeItem(inKey);
-    },
-    clearAll: function () {
-        global[Store.engine].clear();
+;(function () {
+  'use strict'
+
+  nx.declare('nx.Store', {
+    statics: {
+      engine: 'localStorage',
+      config: function (inOptions) {
+        nx.mix(Store, inOptions)
+      },
+      set: function (inKey, inValue) {
+        global[Store.engine].setItem(inKey, nx.stringify(inValue))
+      },
+      get: function (inKey) {
+        var value = global[Store.engine].getItem(inKey)
+        return nx.parse(value)
+      },
+      sets: function (inObject) {
+        nx.each(inObject, function (key, value) {
+          Store.set(key, value)
+        })
+      },
+      clear: function (inKey) {
+        global[Store.engine].removeItem(inKey)
+      },
+      clearAll: function () {
+        global[Store.engine].clear()
+      }
     }
-  }
-})
+  });
+
+}())
