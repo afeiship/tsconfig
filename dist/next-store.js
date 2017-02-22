ea9c4a6d8e48a7e5e1b9ac6d5172fd410fca2f10
afeiship <1290657123@qq.com>
2017-02-22 10:58:13 +0800

    update dist

--- dist/next-store.js
+++ dist/next-store.js
@@ -1,12 +1,3 @@
-;(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.Next-store = factory();
-  }
-}(this, function() {
 (function () {
   'use strict'
 
@@ -43,6 +34,3 @@
   }
 
 }());
-
-return nx.Store;
-}));
