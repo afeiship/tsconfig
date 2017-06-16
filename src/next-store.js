55e61c463ef1bf99fbcdb92accf355148a111765
afeiship <1290657123@qq.com>
2017-06-16 23:42:39 +0800

    optmize api

--- src/next-store.js
+++ src/next-store.js
@@ -1,91 +1,66 @@
 (function () {
 
-  var global = global || this;
+  var global = global || window || this;
   var nx = global.nx || require('next-js-core2');
+  var NxLocalStorage = nx.LocalStorage || require('next-local-storage');
+  var NxSessionStorage = nx.SessionStorage || require('next-session-storage');
 
   //engie list:
   var ENGIE_LOCAL = 'localStorage';
   var ENGIE_SESSION = 'sessionStorage';
 
-  var Store = nx.declare('nx.Store', {
+  var NxStore = nx.declare('nx.Store', {
     properties:{
       local:{
         get:function(){
-          this.engine = ENGIE_LOCAL;
-          return this.gets();
+          return this._localStorage.gets();
         },
         set: function(inValue){
-          this.engine = ENGIE_LOCAL;
-          this.sets(inValue);
+          this._localStorage.sets(inValue);
         }
       },
       session:{
         get:function(){
-          this.engine = ENGIE_SESSION;
-          return this.gets();
+          this._sessionStorage.gets();
         },
         set: function(inValue){
-          this.engine = ENGIE_SESSION;
-          this.sets(inValue);
+          this._sessionStorage.sets(inValue);
         }
       }
     },
-    statics: {
+    statics:{
       engine: ENGIE_LOCAL,
-      prefix: '',
-      config: function (inOptions) {
-        nx.mix(Store, inOptions);
-      },
-      set: function (inKey, inValue) {
-        global[Store.engine].setItem(Store.__key(inKey), nx.stringify(inValue));
-      },
-      get: function (inKey) {
-        var value = global[Store.engine].getItem(Store.__key(inKey));
-        return nx.parse(value);
-      },
-      sets: function (inObject) {
-        nx.each(inObject, function (key, value) {
-          Store.set(key, value);
-        });
-      },
-      gets: function(inKeys){
-        var result={};
-        var keys = inKeys || [];
-        var i = 0, key;
-        var storeEngine = global[Store.engine];
-        if(keys.length == 0){
-          for (i = 0; i < storeEngine.length; i++)   {
-            key = storeEngine.key(i);
-            keys.push(key);
-          }
-        }
+      init: function(){
+        this._localStorage = new NxLocalStorage();
+        this._sessionStorage = new NxSessionStorage();
 
-        nx.each(keys,function(i,key){
-          result[key]=Store.get(key);
-        });
-        return result;
+        //delegates methods:
+        nx.each([
+          'set',
+          'sets',
+          'get',
+          'gets',
+          'clear',
+          'clears'
+        ],function(_,name){
+          var self = this;
+          this[name] = function(inValue){
+            return self['_' + this.engine][name](inValue);
+          };
+        },this);
       },
-      clear: function (inKey) {
-        global[Store.engine].removeItem(inKey);
-      },
-      clearAll: function (inArray) {
-        if(Array.isArray(inArray)){
-          inArray.forEach(function(item){
-            Store.clear(item);
-          })
-        }else{
-          global[Store.engine].clear();
-        }
-      },
-      __key:function (inKey){
-        var prefix = Store.prefix;
-        return prefix ? [prefix,'.',inKey].join('') : inKey;
+      /**
+       * @depreate use 'clears' instead
+       */
+      clearAll: function(inValue){
+        console.warn('Depreated! Please use `clears` methods instead ASAP.');
+        this.clears(inValue);
       }
     }
   });
 
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Store;
+    module.exports = NxStore;
   }
 
 }());
