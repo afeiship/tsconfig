f4397be6e9c51c24be5471cf8b879e473390f12a
afeiship <afeiship@gmail.com>
2019-01-06 14:40:47 +0800

    refactor: add pretieer & remove deprate warning

--- src/next-store.js
+++ src/next-store.js
@@ -1,60 +1,50 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var NxLocalStorage = nx.LocalStorage || require('next-local-storage');
   var NxSessionStorage = nx.SessionStorage || require('next-session-storage');
+  var POPULATE_METHODS = ['set', 'sets', 'get', 'gets', 'clear', 'clears'];
 
   //engie list:
   var ENGIE_LOCAL = 'localStorage';
   var ENGIE_SESSION = 'sessionStorage';
 
   var NxStore = nx.declare('nx.Store', {
-    properties:{
-      local:{
-        get: function(){
+    properties: {
+      local: {
+        get: function() {
           return this._localStorage.gets();
         },
-        set: function(inValue){
+        set: function(inValue) {
           this._localStorage.sets(inValue);
         }
       },
-      session:{
-        get: function(){
+      session: {
+        get: function() {
           return this._sessionStorage.gets();
         },
-        set: function(inValue){
+        set: function(inValue) {
           this._sessionStorage.sets(inValue);
         }
       }
     },
-    statics:{
+    statics: {
       engine: ENGIE_LOCAL,
-      config: function (inPrefix){
+      config: function(inPrefix) {
         this._localStorage = new NxLocalStorage(inPrefix);
         this._sessionStorage = new NxSessionStorage(inPrefix);
 
         //populate methods:
-        nx.each([
-          'set',
-          'sets',
-          'get',
-          'gets',
-          'clear',
-          'clears'
-        ],function(_,name){
-          var self = this;
-          this[name] = function(inName,inValue){
-            return self['_' + self.engine][name](inName,inValue);
-          };
-        },this);
-      },
-      /**
-       * @depreate use 'clears' instead
-       */
-      clearAll: function(inValue){
-        console.warn('Depreated! Please use `clears` methods instead ASAP.');
-        this.clears(inValue);
+        nx.each(
+          POPULATE_METHODS,
+          function(_, name) {
+            var self = this;
+            this[name] = function(inKey, inValue) {
+              return self['_' + self.engine][name](inKey, inValue);
+            };
+          },
+          this
+        );
       }
     }
   });
@@ -62,5 +52,4 @@
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = NxStore;
   }
-
-}());
+})();
